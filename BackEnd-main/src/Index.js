8d49d3df2992f4d10b5e46539a0425274233487a
Anikrawat <132670891+Anikrawat@users.noreply.github.com>
2024-07-27 18:32:20 +0530

    Update Index.js

--- BackEnd-main/src/Index.js
+++ BackEnd-main/src/Index.js
@@ -7,7 +7,7 @@ require('../Models/dataBase')
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin:["https://deploy-mern-1whq.vercel.app"],
+    origin:["to-do-list-frontend-green.vercel.app"],
     methods:['POST','GET'],
     credentials:true
 }));
