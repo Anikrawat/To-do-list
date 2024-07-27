e08a61b13eb93ea9bffc8f55aba2d12c466265e0
Anikrawat <132670891+Anikrawat@users.noreply.github.com>
2024-07-27 19:07:18 +0530

    Update Index.js

--- BackEnd-main/src/Index.js
+++ BackEnd-main/src/Index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
     origin: 'https://to-do-list-frontend-green.vercel.app',
-    methods:['POST','GET'],
+    methods:['POST','GET','PATCH','DELETE'],
     credentials:true
 }));
 app.use(express.json())
