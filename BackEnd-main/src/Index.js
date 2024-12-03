61f3cfea9d0b05c449b02b2fe5747abd22d69e28
Anikrawat <132670891+Anikrawat@users.noreply.github.com>
2024-12-03 19:35:21 +0530

    Update Index.js

--- BackEnd-main/src/Index.js
+++ BackEnd-main/src/Index.js
@@ -6,13 +6,16 @@ require('../Models/dataBase')
 
 const app = express()
 const port = process.env.PORT || 5000
-app.use(cors({
+const corsOptions = {
     origin: 'https://to-do-list-frontend-green.vercel.app',
-    methods:['POST','GET','PATCH','DELETE'],
-    credentials:true
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 
-app.options('*', cors());
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 app.use(express.json())
 
 app.use('/api/auth',require('../Routes/Auth'))
